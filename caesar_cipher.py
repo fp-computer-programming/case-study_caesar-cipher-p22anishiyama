48ccccdfca1a5960a0698993172172e1c1ff313b
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-04-28 16:42:57 -0400

    i am so incredibly confused with this

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,6 +3,13 @@
 # Imports
 from string import ascii_uppercase
 
+# Main
+
+# defining the two variables as inputs
+user_file = input("Please enter a file to be encrypted: ")
+shift_value = input("Please enter a shift value: ")
+
+# opening the text to be accessed
 original_file = open('alma_mater.txt')
 line = original_file.readline().strip()
 
@@ -14,19 +21,31 @@ def cipher_key(shift):
     return dict(zip(original_letters, shifted_letters))
 
 
+# opening the text as a file and reading the lines
+with open(user_file, 'r') as my_file:
+    lines = my_file.readlines()
+    for x in lines:
+        line = x
+
+
+
+# translating each line 
 def shift_line(line, dict_key):
     new_line = ""
-    # Add code here
+    for x in line:
+        for (key, val) in dict_key.items():
+            if key == x:
+                new_line += x
     return new_line
 
 
+# appending each line that has been translated to its corresponding area
 def encrypt_message(filename, dict_key):
-    # Add code here
-    pass
 
-# Main
-user_file = input("Please enter a file to be encrypted: ")
-shift_value = input("Please enter a shift value: ")
+    new_file = open(filename, "w")
+    new_file.write(new_line + "\n")
+    new_file.close()
+
 
 key = cipher_key(shift_value)
 
